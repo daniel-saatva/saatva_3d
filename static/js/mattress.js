07fa0433302de12954f26bf584228a7e61fbd8b8
Manfred Marin Hernandez <mmarin@saatvamattress.com>
2021-09-22 14:14:35 -0600

    add mattress gltf model and display it

--- static/js/mattress.js
+++ static/js/mattress.js
@@ -1,26 +1,38 @@
+let mattress;
+const renderer = new THREE.WebGLRenderer();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const gltfLoader = new THREE.GLTFLoader();
 
-const renderer = new THREE.WebGLRenderer();
-const image_slider = document.getElementById('image-slider');
-image_slider.innerHTML = "";
-renderer.setSize(image_slider.offsetWidth , image_slider.offsetHeight );
-image_slider.appendChild( renderer.domElement );
+function init() {
+    gltfLoader.load('/mattress_nologo.gltf', function ( gltf ) {
+        mattress = gltf.scene;
+        scene.add( gltf.scene );
+    }, undefined, function ( error ) {
+        console.error( error );
+    });
 
-const geometry = new THREE.BoxGeometry(2,2,2,2);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    const light1 = new THREE.AmbientLight( 0xffffff, 1 );
+    scene.add( light1 );
+    const light2 = new THREE.PointLight( 0xffffff, 0.5 );
+    scene.add( light2 );
 
-camera.position.z = 5;
+    const image_slider = document.getElementById('image-slider');
+    image_slider.innerHTML = "";
+    renderer.setClearColor( 0xffffff );
+    renderer.setSize(image_slider.offsetWidth , image_slider.offsetHeight );
+    image_slider.appendChild( renderer.domElement );
+
+    camera.position.z = 20;
+    camera.position.y = 3;
+
+    animate();
+}
 
 const animate = function () {
     requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
+    mattress.rotation.y += 0.01;
     renderer.render( scene, camera );
 };
 
-animate();
+window.onload = init
